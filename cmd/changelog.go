ec06cfe623f90a5090cd23c65255f82681a4dc45
Chau Vu <cvu@chanzuckerberg.com>
2019-06-07 10:10:52 -0700

    Add changelog_test.go

--- cmd/changelog.go
+++ cmd/changelog.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
+
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
@@ -21,28 +26,80 @@ var changelogCmd = &cobra.Command{
 		if err != nil {
 			return errors.Wrap(err, "could not open git repo")
 		}
-
-		// TODO fill these out
-		// git describe --abbrev=0 --tags
-		fetchOptions := &git.FetchOptions{}
-		err = repo.Fetch(fetchOptions)
-
-		// TODO: fill these out
-		//git log $(git describe --abbrev=0 --tags)..HEAD --pretty=format:"* [%h](../../commit/%H) %s"
-		options := &git.LogOptions{
-			// From:
-		}
-
-		gitLog, err := repo.Log(options)
+		tagCommitHash, err := LatestTagCommitHash(repo)
+		fmt.Printf("Last tagCommitHash: %s\n", tagCommitHash)
 		if err != nil {
-			return errors.Wrap(err, "Could not fetch git log")
+			return errors.New("unable to retrieve latest tag's commit hash")
 		}
 
-		err = gitLog.ForEach(func(commit *object.Commit) error {
-			// TODO do something for these git log entities
-			return nil
+		cIter, err := repo.Log(&git.LogOptions{
+			All:   true,
+			Order: git.LogOrderCommitterTime,
 		})
+		if err != nil {
+			return errors.Wrap(err, "failed to retrieve commit history")
+		}
 
+		var changelog bytes.Buffer
+		curCommit, err := cIter.Next()
+		fmt.Printf("first curCommit %s\n", &curCommit.Hash)
+		for &curCommit.Hash == tagCommitHash {
+			changelog.WriteString(GetCommitLog(curCommit))
+			curCommit, err = cIter.Next()
+		}
+		fmt.Println(changelog.String())
+		// if tagCommitHash == nil {
+		// 	// no tag found for this repo, get all commits
+		// 	cIter, err = repo.Log(&git.LogOptions{
+		// 		All:   true,
+		// 		Order: git.LogOrderCommitterTime,
+		// 	})
+		// } else {
+		// 	cIter, err = repo.Log(&git.LogOptions{
+		// 		From:  *tagCommitHash,
+		// 		Order: git.LogOrderCommitterTime,
+		// 	})
+		// }
+		// if err != nil {
+		// 	return errors.Wrap(err, "failed to retrieve commit history")
+		// }
+		// var changelog bytes.Buffer
+		// err = cIter.ForEach(func(commit *object.Commit) error {
+		// 	changelog.WriteString(GetCommitLog(commit))
+		// 	return nil
+		// })
+		// fmt.Println(changelog.String())
 		return nil
 	},
+}
+
+// LatestTagCommitHash will return commit hash of the latest tag if any, and nil if no tag is found
+func LatestTagCommitHash(repo *git.Repository) (*plumbing.Hash, error) {
+	tagRefs, err := repo.Tags()
+	var lastTag plumbing.Revision
+	err = tagRefs.ForEach(func(tagRef *plumbing.Reference) error {
+		curTag := plumbing.Revision(tagRef.Name().String())
+		fmt.Printf("curTag: %s", curTag)
+		if lastTag < curTag {
+			lastTag = curTag
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, errors.New("unable to retrieve tags")
+	}
+	if lastTag == "" {
+		return nil, nil
+	}
+	return repo.ResolveRevision(lastTag)
+}
+
+// GetCommitLog takes a commit object and returns a commit log, for example:
+// * [2847a2e6](../../commit/2847a2e624ee6736b43cc3a68acd75168d1a75d6) A commit message
+func GetCommitLog(commit *object.Commit) string {
+	hash := commit.Hash.String()
+	if hash != "" {
+		return fmt.Sprintf("* [%s](../../commit/%s) %s\n", hash[:8], hash, strings.Split(commit.Message, "\n")[0])
+	}
+	return ""
 }
---
+++ cmd/changelog_test.go
@@ -0,0 +1,48 @@
+package cmd_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/chanzuckerberg/bff/cmd"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+)
+
+func TestGetCommitLog(t *testing.T) {
+	tests := []struct {
+		name   string
+		commit object.Commit
+		want   string
+	}{
+		{"non-empty commit, one-line commit message",
+			object.Commit{
+				Hash: [20]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+				// Message: "A commit message\nWith multiple lines\nLast line.",
+				Message: "A commit message",
+			},
+			"* [00010203](../../commit/0001020304050607080900010203040506070809) A commit message\n",
+		},
+		{"non-empty commit, multiple line commit message",
+			object.Commit{
+				Hash:    [20]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+				Message: "A commit message\nWith multiple lines\nLast line.",
+			},
+			"* [00010203](../../commit/0001020304050607080900010203040506070809) A commit message\n",
+		},
+		// {"How to test commit == nil?",
+		// 	object.Commit{
+		// 		Hash:    [20]byte{},
+		// 		Message: "",
+		// 	},
+		// 	"",
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cmd.GetCommitLog(&tt.commit); got != tt.want {
+				fmt.Println(got)
+				t.Errorf("GetCommitLog() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
